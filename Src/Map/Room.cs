736647fa711e336576fb923658f272f234420e74
George Claudiu Ilief <ilief.claudiu@gmail.com>
2025-04-02 01:27:00 -0700

    Add project files.

---
+++ Src/Map/Room.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace OOPAdventure;
+
+public class Room
+{
+    public string Name { get; set; } = Text.Language.DefaultRoomName;
+    public string Description { get; set; } = Text.Language.DefaultRoomDescription;
+
+    public Dictionary<Directions, int> Neighbors { get; set; } = new()
+    {
+        {Directions.North, -1},
+        {Directions.South, -1},
+        {Directions.East, -1},
+        {Directions.West, -1},
+        {Directions.None, -1},
+    };
+    public bool Visited {  get; set; }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        if (Visited)
+            sb.Append(string.Format(Text.Language.RoomOld, Name));
+        else
+            sb.Append(string.Format(Text.Language.RoomNew, Name));
+
+       var names = Enum.GetNames(typeof(Directions));
+        var directions = (from p in names where Neighbors[(Directions)Enum.Parse(typeof(Directions), p)] > -1 select p.ToLower().ToArray());
+        var description = string.Format(Description, Text.Language.JoinedWordList(directions, Text.Language.And));
+        sb.Append(description);
+        return sb.ToString();
+    }
+}
---
+++ Src/Text/Language.cs
@@ -0,0 +1,24 @@
+﻿namespace OOPAdventure;
+
+public abstract partial class Language
+{
+    public string Welcome { get; protected set; } = "";
+    public string ChooseYourName { get; protected set; } = "";
+    public string DefaultName { get; protected set; } = "";
+    public string DefaultRoomDescription { get; protected set; } = "";
+    public string DefaultRoomName { get; protected set; } = "";
+    public string ActionError { get; protected set; } = "";
+    public string Go { get; protected set; } = "";
+    public string GoError { get; protected set; } = "";
+    public string WhatToDo { get; protected set; } = "";
+    public string Quit { get; protected set; } = "";
+    public string RoomNew { get; protected set; } = "";
+    public string RoomOld { get; protected set; } = "";
+    public string And { get; protected set; } = "";
+    public string Comma { get; protected set; } = "";
+    public string Space { get; protected set; } = "";
+
+
+
+
+}
